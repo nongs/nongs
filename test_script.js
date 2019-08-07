e7cb49b8e470be4d15f6d0c9e8f37684fd355096
nongs <fovostra@gmail.com>
2019-08-08 07:07:27 +0900

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -1,18 +1,27 @@
 /*** 구현 함수 : *카테고리 탭 리스트 생성 ***/
+var getData;
+var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        getData = JSON.parse(this.responseText);
+        console.log("Json parsed data is: " + JSON.stringify(getData));
+       }
+    };
+xmlhttp.open("GET", "test_data.json", true);
+xmlhttp.send();
+
+const e_c = React.createElement;
 
 function CategoryList(props) {
-  const categorys = this.props.category;
+  const categorys = props.category;
   const categoryItem = categorys.map((category, index) =>
-    <li key={index} tabIndex="0">
-      {category.tab_name}
-    </li>
+    e_c('li', {key: index}, category.tab_name)
   );
-    return (
-      <ul>{categoryItem}</ul>
-      );
+
+  return e_c('ul', null, categoryItem);
 }
 
 ReactDOM.render(
-  <CategoryList category={getData.result} />,
+  e_c(CategoryList, {category: getData.result}, null),
   document.getElementById('contentArea')
 );
