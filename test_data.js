4a4937810a20144c0d6e3350bbc5ea4abd4d8bc5
nongs <fovostra@gmail.com>
2019-08-08 06:00:18 +0900

    Create test_data.js

---
+++ test_data.js
@@ -0,0 +1,403 @@
+const getData = {
+  "result": [
+        {
+            "tab_name": "여행명소",
+            "data": [
+                {
+                    "city_id": "-240905",
+                    "cd_idx": 1302647,
+                    "ad_rating": 0,
+                    "waug_id": 107075,
+                    "images": [
+                        {
+                            "cd_idx": "1302647",
+                            "url": "https://eb.mycelebs.com/activity/new/sq/1302647_sq_00.jpg?update_date=2019-02-13T11:00:49Z",
+                            "update_date": "2019-02-13T11:00:49Z"
+                        }
+                    ],
+                    "price": {
+                        "currency": "KRW",
+                        "currency_symbol": "₩",
+                        "currency_symbol_translate": "원",
+                        "price": "35,236"
+                    },
+                    "price_exist": 1,
+                    "ad_name": "유니버셜 익스프레스™패스 에반게리온 XR라이드",
+                    "city_name": "오사카",
+                    "ad_name_f": "유니버셜 익스프레스™패스 에반게리온 XR라이드",
+                    "country_name": "일본",
+                    "ad_main_category": [
+                        "여행명소"
+                    ],
+                    "cd_name": "유니버셜 익스프레스™패스 에반게리온 XR라이드",
+                    "url": "/activity/1302647",
+                    "url_encode": "/activity/1302647",
+                    "vertical": "activity",
+                    "profile_url": "https://www.waug.com/good/?idx=107075",
+                    "vcfr_filter_ranking_data": [],
+                    "title": "유니버셜 익스프레스™패스 에반게리온 XR라이드",
+                    "sub_title": "여행명소 | 일본, 오사카",
+                    "review_ment": "평점 0.0"
+                },
+                {
+                    "city_id": "-240905",
+                    "cd_idx": 1301892,
+                    "ad_rating": 3.7,
+                    "waug_id": 106972,
+                    "images": [
+                        {
+                            "cd_idx": "1301892",
+                            "url": "https://eb.mycelebs.com/activity/new/sq/1301892_sq_00.jpg?update_date=2019-02-13T10:57:26Z",
+                            "update_date": "2019-02-13T10:57:26Z"
+                        }
+                    ],
+                    "price": {
+                        "currency": "KRW",
+                        "currency_symbol": "₩",
+                        "currency_symbol_translate": "원",
+                        "price": "84,336"
+                    },
+                    "price_exist": 1,
+                    "ad_name": "유니버셜스튜디오재팬™ & JR 간사이패스",
+                    "city_name": "오사카",
+                    "ad_name_f": "유니버셜스튜디오재팬™ & JR 간사이패스",
+                    "country_name": "일본",
+                    "ad_main_category": [
+                        "여행명소"
+                    ],
+                    "cd_name": "유니버셜스튜디오재팬™ & JR 간사이패스",
+                    "url": "/activity/1301892",
+                    "url_encode": "/activity/1301892",
+                    "vertical": "activity",
+                    "profile_url": "https://www.waug.com/good/?idx=106972",
+                    "vcfr_filter_ranking_data": [],
+                    "title": "유니버셜스튜디오재팬™ & JR 간사이패스",
+                    "sub_title": "여행명소 | 일본, 오사카",
+                    "review_ment": "평점 3.7"
+                },
+                {
+                    "city_id": "-240905",
+                    "cd_idx": 1301621,
+                    "ad_rating": 5,
+                    "waug_id": 106896,
+                    "images": [
+                        {
+                            "cd_idx": "1301621",
+                            "url": "https://eb.mycelebs.com/activity/new/sq/1301621_sq_00.jpg?update_date=2019-02-13T11:00:19Z",
+                            "update_date": "2019-02-13T11:00:19Z"
+                        }
+                    ],
+                    "price": {
+                        "currency": "KRW",
+                        "currency_symbol": "₩",
+                        "currency_symbol_translate": "원",
+                        "price": "47,389"
+                    },
+                    "price_exist": 1,
+                    "ad_name": "유니버셜 스튜디오 재팬™ 쇼핑 쿠폰",
+                    "city_name": "오사카",
+                    "ad_name_f": "유니버셜 스튜디오 재팬™ 쇼핑 쿠폰",
+                    "country_name": "일본",
+                    "ad_main_category": [
+                        "여행명소"
+                    ],
+                    "cd_name": "유니버셜 스튜디오 재팬™ 쇼핑 쿠폰",
+                    "url": "/activity/1301621",
+                    "url_encode": "/activity/1301621",
+                    "vertical": "activity",
+                    "profile_url": "https://www.waug.com/good/?idx=106896",
+                    "vcfr_filter_ranking_data": [],
+                    "title": "유니버셜 스튜디오 재팬™ 쇼핑 쿠폰",
+                    "sub_title": "여행명소 | 일본, 오사카",
+                    "review_ment": "평점 5.0"
+                },
+                {
+                    "city_id": "-240905",
+                    "cd_idx": 1300986,
+                    "ad_rating": 5,
+                    "waug_id": 105264,
+                    "images": [
+                        {
+                            "cd_idx": "1300986",
+                            "url": "https://eb.mycelebs.com/activity/new/sq/1300986_sq_00.jpg?update_date=2019-02-13T10:55:33Z",
+                            "update_date": "2019-02-13T10:55:33Z"
+                        }
+                    ],
+                    "price": {
+                        "currency": "KRW",
+                        "currency_symbol": "₩",
+                        "currency_symbol_translate": "원",
+                        "price": "334,688"
+                    },
+                    "price_exist": 1,
+                    "ad_name": "USJ 로열 스튜디오 패스 (일본공항수령)",
+                    "city_name": "오사카",
+                    "ad_name_f": "USJ 로열 스튜디오 패스 (일본공항수령)",
+                    "country_name": "일본",
+                    "ad_main_category": [
+                        "여행명소"
+                    ],
+                    "cd_name": "USJ 로열 스튜디오 패스 (일본공항수령)",
+                    "url": "/activity/1300986",
+                    "url_encode": "/activity/1300986",
+                    "vertical": "activity",
+                    "profile_url": "https://www.waug.com/good/?idx=105264",
+                    "vcfr_filter_ranking_data": [],
+                    "title": "USJ 로열 스튜디오 패스 (일본공항수령)",
+                    "sub_title": "여행명소 | 일본, 오사카",
+                    "review_ment": "평점 5.0"
+                },
+                {
+                    "city_id": "-240905",
+                    "cd_idx": 1302080,
+                    "ad_rating": 0,
+                    "waug_id": 107045,
+                    "images": [
+                        {
+                            "cd_idx": "1302080",
+                            "url": "https://eb.mycelebs.com/activity/new/sq/1302080_sq_00.jpg?update_date=2019-02-13T10:58:16Z",
+                            "update_date": "2019-02-13T10:58:16Z"
+                        }
+                    ],
+                    "price": {
+                        "currency": "KRW",
+                        "currency_symbol": "₩",
+                        "currency_symbol_translate": "원",
+                        "price": "18,828"
+                    },
+                    "price_exist": 1,
+                    "ad_name": "오사카 니후레루(Nifrel) 티켓",
+                    "city_name": "오사카",
+                    "ad_name_f": "오사카 니후레루(Nifrel) 티켓",
+                    "country_name": "일본",
+                    "ad_main_category": [
+                        "여행명소"
+                    ],
+                    "cd_name": "오사카 니후레루(Nifrel) 티켓",
+                    "url": "/activity/1302080",
+                    "url_encode": "/activity/1302080",
+                    "vertical": "activity",
+                    "profile_url": "https://www.waug.com/good/?idx=107045",
+                    "vcfr_filter_ranking_data": [],
+                    "title": "오사카 니후레루(Nifrel) 티켓",
+                    "sub_title": "여행명소 | 일본, 오사카",
+                    "review_ment": "평점 0.0"
+                },
+                {
+                    "city_id": "-240905",
+                    "cd_idx": 1300320,
+                    "ad_rating": 4.4,
+                    "waug_id": 104293,
+                    "images": [
+                        {
+                            "cd_idx": "1300320",
+                            "url": "https://eb.mycelebs.com/activity/new/sq/1300320_sq_00.jpg?update_date=2019-02-13T10:53:40Z",
+                            "update_date": "2019-02-13T10:53:40Z"
+                        }
+                    ],
+                    "price": {
+                        "currency": "KRW",
+                        "currency_symbol": "₩",
+                        "currency_symbol_translate": "원",
+                        "price": "25,252"
+                    },
+                    "price_exist": 1,
+                    "ad_name": "오사카 주유패스 1/2일권",
+                    "city_name": "오사카",
+                    "ad_name_f": "오사카 주유패스 1/2일권",
+                    "country_name": "일본",
+                    "ad_main_category": [
+                        "여행명소"
+                    ],
+                    "cd_name": "오사카 주유패스 1/2일권",
+                    "url": "/activity/1300320",
+                    "url_encode": "/activity/1300320",
+                    "vertical": "activity",
+                    "profile_url": "https://www.waug.com/good/?idx=104293",
+                    "vcfr_filter_ranking_data": [],
+                    "title": "오사카 주유패스 1/2일권",
+                    "sub_title": "여행명소 | 일본, 오사카",
+                    "review_ment": "평점 4.4"
+                }
+            ],
+            "deep_link_param": {
+                "ad_main_category_ko": [
+                    "여행명소"
+                ],
+                "name": "여행명소",
+                "vertical": "activity",
+                "item_key": "meta_여행명소"
+            }
+        },
+        {
+            "tab_name": "이색체험",
+            "data": [
+                {
+                    "city_id": "-240905",
+                    "cd_idx": 1301419,
+                    "ad_rating": 5,
+                    "waug_id": 105303,
+                    "images": [
+                        {
+                            "cd_idx": "1301419",
+                            "url": "https://eb.mycelebs.com/activity/new/sq/1301419_sq_00.jpg?update_date=2019-02-13T10:56:08Z",
+                            "update_date": "2019-02-13T10:56:08Z"
+                        }
+                    ],
+                    "price": {
+                        "currency": "KRW",
+                        "currency_symbol": "₩",
+                        "currency_symbol_translate": "원",
+                        "price": "13,805"
+                    },
+                    "price_exist": 1,
+                    "ad_name": "오사카 원더루프 버스 2일권",
+                    "city_name": "오사카",
+                    "ad_name_f": "오사카 원더루프 버스 2일권",
+                    "country_name": "일본",
+                    "ad_main_category": [
+                        "이색체험"
+                    ],
+                    "cd_name": "오사카 원더루프 버스 2일권",
+                    "url": "/activity/1301419",
+                    "url_encode": "/activity/1301419",
+                    "vertical": "activity",
+                    "profile_url": "https://www.waug.com/good/?idx=105303",
+                    "vcfr_filter_ranking_data": [],
+                    "title": "오사카 원더루프 버스 2일권",
+                    "sub_title": "이색체험 | 일본, 오사카",
+                    "review_ment": "평점 5.0"
+                }
+            ],
+            "deep_link_param": {
+                "ad_main_category_ko": [
+                    "이색체험"
+                ],
+                "name": "이색체험",
+                "vertical": "activity",
+                "item_key": "meta_이색체험"
+            }
+        },
+        {
+            "tab_name": "다이닝",
+            "data": [
+                {
+                    "city_id": "-240905",
+                    "cd_idx": 1301515,
+                    "ad_rating": 3.7,
+                    "waug_id": 106004,
+                    "images": [
+                        {
+                            "cd_idx": "1301515",
+                            "url": "https://eb.mycelebs.com/activity/new/sq/1301515_sq_00.jpg?update_date=2019-02-13T10:58:22Z",
+                            "update_date": "2019-02-13T10:58:22Z"
+                        }
+                    ],
+                    "price": {
+                        "currency": "KRW",
+                        "currency_symbol": "₩",
+                        "currency_symbol_translate": "원",
+                        "price": "32,831"
+                    },
+                    "price_exist": 1,
+                    "ad_name": "오사카 타코노테츠 타코야키",
+                    "city_name": "오사카",
+                    "ad_name_f": "오사카 타코노테츠 타코야키",
+                    "country_name": "일본",
+                    "ad_main_category": [
+                        "다이닝"
+                    ],
+                    "cd_name": "오사카 타코노테츠 타코야키",
+                    "url": "/activity/1301515",
+                    "url_encode": "/activity/1301515",
+                    "vertical": "activity",
+                    "profile_url": "https://www.waug.com/good/?idx=106004",
+                    "vcfr_filter_ranking_data": [],
+                    "title": "오사카 타코노테츠 타코야키",
+                    "sub_title": "다이닝 | 일본, 오사카",
+                    "review_ment": "평점 3.7"
+                },
+                {
+                    "city_id": "-240905",
+                    "cd_idx": 1301777,
+                    "ad_rating": 0,
+                    "waug_id": 107153,
+                    "images": [
+                        {
+                            "cd_idx": "1301777",
+                            "url": "https://eb.mycelebs.com/activity/new/sq/1301777_sq_00.jpg?update_date=2019-02-13T10:58:39Z",
+                            "update_date": "2019-02-13T10:58:39Z"
+                        }
+                    ],
+                    "price": {
+                        "currency": "KRW",
+                        "currency_symbol": "₩",
+                        "currency_symbol_translate": "원",
+                        "price": "15,040"
+                    },
+                    "price_exist": 1,
+                    "ad_name": "쿠시카츠 대통령 무한리필",
+                    "city_name": "오사카",
+                    "ad_name_f": "쿠시카츠 대통령 무한리필",
+                    "country_name": "일본",
+                    "ad_main_category": [
+                        "다이닝"
+                    ],
+                    "cd_name": "쿠시카츠 대통령 무한리필",
+                    "url": "/activity/1301777",
+                    "url_encode": "/activity/1301777",
+                    "vertical": "activity",
+                    "profile_url": "https://www.waug.com/good/?idx=107153",
+                    "vcfr_filter_ranking_data": [],
+                    "title": "쿠시카츠 대통령 무한리필",
+                    "sub_title": "다이닝 | 일본, 오사카",
+                    "review_ment": "평점 0.0"
+                },
+                {
+                    "city_id": "-240905",
+                    "cd_idx": 1301859,
+                    "ad_rating": 0,
+                    "waug_id": 106724,
+                    "images": [
+                        {
+                            "cd_idx": "1301859",
+                            "url": "https://eb.mycelebs.com/activity/new/sq/1301859_sq_00.jpg?update_date=2019-02-13T11:02:42Z",
+                            "update_date": "2019-02-13T11:02:42Z"
+                        }
+                    ],
+                    "price": {
+                        "currency": "KRW",
+                        "currency_symbol": "₩",
+                        "currency_symbol_translate": "원",
+                        "price": "81,637"
+                    },
+                    "price_exist": 1,
+                    "ad_name": "고베규 무한리필 규이치",
+                    "city_name": "오사카",
+                    "ad_name_f": "고베규 무한리필 규이치",
+                    "country_name": "일본",
+                    "ad_main_category": [
+                        "다이닝"
+                    ],
+                    "cd_name": "고베규 무한리필 규이치",
+                    "url": "/activity/1301859",
+                    "url_encode": "/activity/1301859",
+                    "vertical": "activity",
+                    "profile_url": "https://www.waug.com/good/?idx=106724",
+                    "vcfr_filter_ranking_data": [],
+                    "title": "고베규 무한리필 규이치",
+                    "sub_title": "다이닝 | 일본, 오사카",
+                    "review_ment": "평점 0.0"
+                }
+            ],
+            "deep_link_param": {
+                "ad_main_category_ko": [
+                    "다이닝"
+                ],
+                "name": "다이닝",
+                "vertical": "activity",
+                "item_key": "meta_다이닝"
+            }
+        }
+    ]
+}
