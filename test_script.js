e0a4c42c053c90247b704260fee7c4e6bb397e1e
nongs <fovostra@gmail.com>
2019-08-08 06:04:50 +0900

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -1,12 +1,6 @@
 /*** 구현 함수 : *카테고리 탭 리스트 생성 ***/
 
-class CategoryList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
+function CategoryList(props) {
   const categorys = this.props.category;
   const categoryItem = categorys.map((category, index) =>
     <li key={index} tabIndex="0">
